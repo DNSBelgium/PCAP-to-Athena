1e6ad22877e5f966ba72168bb85a297479fe4770
Quentin Loos <quentin.loos@dnsbelgium.be>
2019-05-28 07:33:04 +0200

    Add copyright notice

--- src/main/java/be/dnsbelgium/data/pcap/convertor/ConvertorConfig.java
+++ src/main/java/be/dnsbelgium/data/pcap/convertor/ConvertorConfig.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of PCAP to Athena.
+ *
+ * Copyright (c) 2019 DNS Belgium.
+ *
+ * PCAP to Athena is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PCAP to Athena is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PCAP to Athena.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package be.dnsbelgium.data.pcap.convertor;
 
 import org.slf4j.Logger;
