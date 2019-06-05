34273a85495ad2e42cbdad11b823ec5921c15a52
Quentin Loos <quentin.loos@dnsbelgium.be>
2019-06-05 07:50:00 +0200

    Do not include tests that require aws credentials in unit tests but in aws integration test (2)

--- src/test/java/be/dnsbelgium/data/pcap/ip/SubnetConfigTest.java
+++ src/test/java/be/dnsbelgium/data/pcap/ip/SubnetConfigTest.java
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
--- src/test/java/be/dnsbelgium/data/pcap/utils/GeoLookupUtilTest.java
+++ src/test/java/be/dnsbelgium/data/pcap/utils/GeoLookupUtilTest.java
@@ -28,6 +28,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
