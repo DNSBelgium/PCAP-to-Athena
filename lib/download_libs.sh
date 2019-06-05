3fb3c4abf21c4336368648b2b208c7cbf447b762
Quentin Loos <quentin.loos@dnsbelgium.be>
2019-06-05 07:08:41 +0200

    PCAP lib depends on DNS lib

--- lib/download_libs.sh
+++ lib/download_libs.sh
@@ -6,11 +6,11 @@ curl https://s3.amazonaws.com/athena-downloads/drivers/JDBC/SimbaAthenaJDBC_2.0.
 # Entrada
 BRANCH=v0.1.3
 
+# dnslib4java
+cd ../ && git checkout $BRANCH -- dnslib4java
+cd dnslib4java && mvn clean install
+
 # pcaplib4java
 git clone --branch $BRANCH --depth 1 --no-checkout --filter=blob:none https://github.com/SIDN/entrada
 cd entrada && git checkout $BRANCH -- pcaplib4java
 cd pcaplib4java && mvn clean install
-
-# dnslib4java
-cd ../ && git checkout $BRANCH -- dnslib4java
-cd dnslib4java && mvn clean install
